b06f88c8e01cc8fa212acb5cddf919cf105ec492
Leif Myer <leifmyer@gmail.com>
2016-08-06 00:28:43 -0700

    Setup Projects

---
+++ Allocators/Allocators/allocators.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace allocators
+{
+	class stack_allocator
+	{
+
+	};
+
+	class pool_allocator
+	{
+
+	};
+
+	class aligned_allocator
+	{
+
+	};
+
+	class double_buffered_allocator
+	{
+
+	};
+}
